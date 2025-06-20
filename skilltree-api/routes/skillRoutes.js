f1455c9967dfa420b3842b3da172537ae65ea32a
Aditi Dewangan <aditdew24@gmail.com>
2025-06-20 12:55:49 +0530

    Initial SkillTree ApI

---
+++ skilltree-api/routes/skillRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Skill = require('../models/Skill');
+
+// GET all skills
+router.get('/', async (req, res) => {
+  const skills = await Skill.find().populate('prerequisiteSkill');
+  res.json(skills);
+});
+
+// POST a new skill
+router.post('/', async (req, res) => {
+  const { name, description, level, prerequisiteSkill } = req.body;
+  const newSkill = new Skill({ name, description, level, prerequisiteSkill });
+  await newSkill.save();
+  res.status(201).json(newSkill);
+});
+
+// PUT update skill
+router.put('/:id', async (req, res) => {
+  const updatedSkill = await Skill.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedSkill);
+});
+
+// DELETE a skill
+router.delete('/:id', async (req, res) => {
+  await Skill.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Skill deleted' });
+});
+
+module.exports = router;
